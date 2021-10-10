d13e83e86adccde1a7f2075f6f87612ee2b12950
Denise F. de Rezende <Denise@DeFRMacAir.local>
2021-10-10 10:44:53 -0300

    Replace ECGFileName with Files
    * Now Files will be a list containing the names of each file

--- APP/good_heart/lib/communication_with_server.dart
+++ APP/good_heart/lib/communication_with_server.dart
@@ -5,45 +5,41 @@ CommunicationWithServer communicationWithServerFromJson(String str) => Communica
 String communicationWithServerToJson(CommunicationWithServer data) => json.encode(data.toJson());
 
 class CommunicationWithServer {
-  CommunicationWithServer({
-    this.IdMsg,
-    this.OpCode,
-    this.ECGTime,
-    this.ECGfile,
-    this.FreqCard,
-    this.GoodComplex,
-    this.BadComplex,
-  });
 
   dynamic IdMsg;
   dynamic OpCode;
   dynamic ECGTime;
-  dynamic ECGfile;
+  dynamic ECGFile;
   dynamic FreqCard;
   dynamic GoodComplex;
   dynamic BadComplex;
+  dynamic Files;
+
+
+  CommunicationWithServer({this.IdMsg, this.OpCode, this.ECGTime, this.ECGFile, this.FreqCard, this.GoodComplex, this.BadComplex, this.Files});
+
+  String toJson() {
+    var data = <String, dynamic>{};
+    data["IdMsg"] = this.IdMsg;
+    data["OpCode"] = this.OpCode;
+    data["ECGTime"] = this.ECGTime;
+    data["ECGFile"] = this.ECGFile;
+    data["FreqCard"] = this.FreqCard;
+    data["GoodComplex"] = this.GoodComplex;
+    data["BadComplex"] = this.BadComplex;
+    data["Files"] = this.Files;
+    return jsonEncode(data);
+  }
 
   factory CommunicationWithServer.fromJson(Map<String, dynamic> json) =>
       CommunicationWithServer(
         IdMsg: json["IdMsg"],
         OpCode: json["OpCode"],
         ECGTime: json["ECGTime"],
-        ECGfile: json["ECGfile"],
+        ECGFile: json["ECGFile"],
         FreqCard: json["FreqCard"],
         GoodComplex: json["GoodComplex"],
         BadComplex: json["BadComplex"],
+        Files: json["Files"],
       );
-        
-
-  Map<String, dynamic> toJson() =>
-      {
-        "IdMsg": IdMsg,
-        "OpCode": OpCode,
-        "ECGTime": ECGTime,
-        "ECGfile": ECGfile,
-        "FreqCard": FreqCard,
-        "GoodComplex": GoodComplex,
-        "BadComplex": BadComplex,
-      };
-
 }
